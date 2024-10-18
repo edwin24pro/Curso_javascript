aa627016a57c5a15237ba3ea781569133f3b849c
Edwin <ramosaguilaredwin7@gmail.com>
2024-10-17 20:27:03 -0500

    Ejercicios de ajvascript terminados
    Uso de condiciones simples

--- 3. EstructuraPrograma/PRACTICA/script.js
+++ 3. EstructuraPrograma/PRACTICA/script.js
@@ -1,4 +1,22 @@
+//Primer ejercicio
 numero = prompt("Escribe el primer numero")
-numerosDos = promp("Escribe el segundo numero")
-numeroMayor = numero > numerosDos || numeroMayor > numerosDos
-if
+numerosDos = prompt("Escribe el segundo numero")
+if (numero>numerosDos){
+    console.log("El primer numero es mayor, ",numero)
+}else{console.log("El segundo numero es mayor, ", numerosDos)}
+if ((+numero)*(+numerosDos)>100){
+    console.log("El producto es mayor a 100")
+}
+
+//segundo ejercicio
+password=prompt("Ingrese la contraseña")
+if (password=="arturito"){
+    console.log("Bienvenido")
+}
+//tercer ejercicio
+numeroPrimo = prompt("Ingrese un numero")
+if (numeroPrimo%2==1 || numeroPrimo==2){
+    console.log("Es un numero primo")
+}else{
+    console.log("no es un numero primo")
+}
