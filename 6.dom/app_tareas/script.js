ef84cf6f0a17b09f79409e41046d515a9a24bd5f
Edwin <ramosaguilaredwin7@gmail.com>
2024-12-10 10:08:43 -0500

    feet:tarea con el uso de DOM
    insersion en el documento html y pagina web con el uso de DOM

--- 6.dom/app_tareas/script.js
+++ 6.dom/app_tareas/script.js
@@ -3,9 +3,18 @@ let buton=document.querySelector("#btn-agregar")
 let ul=document.querySelector("#lista-tareas")
 
 buton.addEventListener("click", ()=>{
-    let li=document.createElement("li")
-    li.textContent=imput.value
-    ul.appendChild(li,`<buton>borrar</buton>`)
-    console.log(imput.value)
-    imput.value=""
+    ul.innerHTML += `
+        <li>
+            ${imput.value}
+            <button class="delete">eliminar</button>
+        </li>
+    `;
+    imput.value = "";
 })
+//boton eliminar
+ul.addEventListener("click", (e) => {
+    // Verifica si el elemento clickeado es un botón con la clase "delete"
+    if (e.target.tagName === "BUTTON" && e.target.classList.contains("delete")) {
+        let li = e.target.parentElement; // Obtén el <li> que contiene el botón
+        ul.removeChild(li); // Elimina el <li> del <ul>
+    }})
